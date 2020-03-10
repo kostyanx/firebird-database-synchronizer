dfe0de27a00f2860575b6a36a27e8d1fd548bbd9
Konstantin Melnikov <k.melnikov@tinkoff.ru>
2020-03-10 16:38:34 +0400

    fix run and run.bat scripts

--- src/dist/run.bat
+++ src/dist/run.bat
@@ -1,3 +1,3 @@
 @echo on
 
-java -Xmx512m -Dlog4j.configurationFile="log4j2.xml" -jar catcab-database-synchronizer-1.0.0.jar
+java -Xmx512m -Dlog4j.configurationFile="log4j2.xml" -jar catcab-database-synchronizer-1.0.0.jar %*
